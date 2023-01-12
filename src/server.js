a2b8660198cb14ad6bf677d5b3bf7200fdc8fb48
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-01-12 11:44:06 -0300

    feat: save database on file fix: database.js file location

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from 'node:http';
-import { Database } from './middlewares/database.js';
+import { Database } from './database.js';
 import { json } from './middlewares/json.js';
 
 const database = new Database()
